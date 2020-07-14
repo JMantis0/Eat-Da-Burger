37cd415faf7a7f255616da61db21415e2afcbf11
JMan <60026263+JMantis0@users.noreply.github.com>
2020-07-14 13:55:33 -0700

    Added post and get routes sandwichController.js

--- controllers/sandwichController.js
+++ controllers/sandwichController.js
@@ -15,4 +15,32 @@ router.get("/", function(req, res) {
   });
 });
 
+router.post("/api/sandwiches", (req, res) => {
+  sandwich.create([
+    "name", "consumed"
+  ], [
+    req.body.name, req.body.consumed
+  ], (result) => {
+    // Send back the id 
+    res.json({ id: result.insertId });
+
+  });
+});
+
+router.put("/api/sandwiches/:id", (res, res) => {
+  const condition = `id = ${req.params.id}`;
+
+  console.log("condition", condition);
+
+  sandwich.update({
+    consumed: req.body.consumed
+  }, condition, (result) => {
+    if(result.changedRows === 0) {
+      // If no rows changed, ID must not exists thus 404
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
 module.exports = router;
